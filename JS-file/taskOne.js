334d0925a4bb8a7599f0515f6770a2d2f3bb4e5e
mb-tousif <meta.block.tousif@gmail.com>
2022-03-09 00:54:49 +0600

    first commit

---
+++ JS-file/taskOne.js
@@ -0,0 +1,24 @@
+const getValue=(id)=>{
+    const emailField=document.getElementById(id).value;
+    return emailField;
+}
+const emailValidator=()=>{
+  let email = getValue("getEmail");
+  const regExpForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  console.log(email);
+  if (regExpForEmail.test(email)) {
+    const msg = document.getElementById("text");
+    msg.innerHTML = `<h3 class="text-green-600">Your Input is Valid</h3>`;
+  } else {
+    const msg = document.getElementById("text");
+    msg.innerHTML = `<h3 class="text-red-600">Input  Valid charecter! </h3>`;
+  }
+  // if (email.match(regExpForEmail)) {
+  //   const msg = document.getElementById("text");
+  //   msg.innerHTML = `<h3 class="text-green-600">Your Input is Valid</h3>`;
+  // } else {
+  //   const msg = document.getElementById("text");
+  //   msg.innerHTML = `<h3 class="text-red-600">Input  Valid charecter! </h3>`;
+  // };
+  document.getElementById("getEmail").value = "";
+}
